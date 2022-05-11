a36a3036fbcdcf5c2a0b9d61db621736af00e811
kiritozero117 <86806057+kiritozero117@users.noreply.github.com>
2022-05-11 18:31:47 -0500

    Add files via upload

---
+++ prograweb/templates/footer.php
@@ -0,0 +1,32 @@
+<footer>
+
+    <div class="row">
+        <div class="col-md-3">
+            <h3>Contáctanos</h3>            
+            <p>
+                <a href="https://www.facebook.com/"><img src="img/facebook.png" alt="facebook" height="50px" width="50px"></a>
+                <a href="https://twitter.com/"><img src="img/twitter.png" alt="twitter" height="50px" width="50px"></a>
+                <a href="https://www.youtube.com/"><img src="img/youtube.png" alt="youtube" height="50px" width="50px"></a>
+            </p>
+            <br>
+            <h5>Email</h5>
+            <h6>romeblog@contactme.com</h6>
+        </div>
+        <div class="col-md-6">
+            <h3>Ubicanos</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30135.69368711977!2d-99.5642541423584!3d19.24050041255212!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85cd8b100600c98d%3A0x50062df6247fdfb3!2sTecNM%20-%20Campus%20Instituto%20Tecnol%C3%B3gico%20de%20Toluca!5e0!3m2!1ses!2smx!4v1637347163293!5m2!1ses!2smx" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+        <div class="col-md-3">           
+            <p><u>Términos y condiciones</u></p>
+        </div>
+    </div>
+    <hr>
+    <h5>2021 Rome Blog <i class="fa fa-registered"></i></h5>
+
+</footer>
+
+
+
+</body>
+
+</html>
