f3fda0b1507e5ea1bbec7a11fbc07cbe6509da51
kiritozero117 <alejandroarcealtamirano@hotmail.com>
2022-05-12 19:33:20 -0500

    Modificacion final

--- prograweb/articulos3.php
+++ prograweb/articulos3.php
@@ -7,7 +7,8 @@
     $pwdBD = "060999";
     $nomBD = "proyectoweb";    
     $db = new mysqli($servidor,$usuarioBD,$pwdBD,$nomBD);    
-    $query = mysqli_query($db, "SELECT id, tema,subtema FROM articulos where id_autor=$id");    
+    $query = mysqli_query($db, "SELECT * FROM articulos where id=$id");   
+    $query1 = mysqli_query($db, "SELECT * FROM mensajes where id_ar=$id");    
     echo $id;
 ?>
 
@@ -32,7 +33,7 @@
     <?php include("./templates/header.php")?>
 
     <div class="text-center">
-            <h1 aling="center"><b><i>Articulos</i></b></h1>            
+            <h1 aling="center"><b><i>Articulo</i></b></h1>            
             <hr class="my-4">
     </div>
 
@@ -47,18 +48,48 @@
                 
             <div id="cartas" class="card">
                 <div class="card-body">
-                    <h5 class="card-title"><?php echo $row['id']; ?></h5>
-                    <p class="card-text"><?php echo $row['tema']; ?></p>
-                    <p class="card-text"><?php echo $row['subtema']; ?></p>
-                    <?php $id_us=$row['id'] ?>
-                    <a href="http://localhost/proyecto-web/prograweb/articulos3.php?var1=<?php echo $id_us ?>" class="btn btn-primary">Leer articulo</a>
+                    <h5 class="card-title"><?php echo "ID: ".$row['id']; ?></h5>
+                    <p class="card-text"><?php echo "ID Autor: ".$row['id_autor']; ?></p>
+                    <p class="card-text"><?php echo "Nombre del autor: ".$row['escritor']; ?></p>
+                    <p class="card-text"><?php echo "Tema: ".$row['tema']; ?></p>
+                    <p class="card-text"><?php echo "Subtema: ".$row['subtema']; ?></p>
+                    <p class="card-text"><?php echo "Estatus: ".$row['estatus']; ?></p>
+                    <p class="card-text"><?php echo "Fecha: ".$row['fecha']; ?></p>
+                    <p class="card-text"><?php echo "Lugar del descubrimiento: ".$row['lugar']; ?></p>
+                    <p class="card-text"><?php echo "Descubridores: ".$row['descu']; ?></p>
+                    <p class="card-text"><?php echo "Notas: ".$row['notas']; ?></p>
+                    <p class="card-text"><?php echo "Premios ganados: ".$row['premios']; ?></p>
+                    <p class="card-text"><?php echo $row['articulo']; ?></p>
                 </div>
             </div>
+            
 
             <?php
             echo "<br>";
             }
             ?>
+             <?php
+            $i = 0;
+          
+            while ($row = mysqli_fetch_array($query1)) {
+                
+                ?>            
+                
+            <div id="cartas" class="card">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo "ID usuario: ".$row['id_us']; ?></h5>
+                    <p class="card-text"><?php echo "ID Articulo: ".$row['id_ar']; ?></p>
+                    <p class="card-text"><?php echo "Mensaje: ".$row['mensaje']; ?></p>
+                    <a href="" class="btn btn-primary">publicar un Mensaje </a>
+                </div>
+            </div>
+            
+
+            <?php
+            echo "<br>";
+            }
+            ?>
+            
             
     </div>
         
